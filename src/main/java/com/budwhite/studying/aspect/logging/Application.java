749f0d584de4d2f45773847c29d59684f5a911c1
Alexander Kriegisch <Alexander@Kriegisch.name>
2023-10-08 14:20:56 +0700

    Fix faulty AJ Maven config, reformat code

--- src/main/java/com/budwhite/studying/aspect/logging/Application.java
+++ src/main/java/com/budwhite/studying/aspect/logging/Application.java
@@ -6,16 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Application {
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+  private static final String GREETING_TARGET = "Pippo";
 
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-    private static final String GREETING_TARGET = "Pippo";
+  private static final FakeService fakeService = new FakeService();
+  private static final AnotherService anotherService = new AnotherService();
 
-    private static final FakeService fakeService = new FakeService();
-    private static final AnotherService anotherService = new AnotherService();
-
-    public static void main(String[] args) {
-        logger.info("Application started");
-        logger.info("Greeting result is '{}'", fakeService.greet());
-        logger.info("Insulting result is '{}'", anotherService.insult(GREETING_TARGET));
-    }
+  public static void main(String[] args) {
+    logger.info("Application started");
+    logger.info("Greeting result is '{}'", fakeService.greet());
+    logger.info("Insulting result is '{}'", anotherService.insult(GREETING_TARGET));
+  }
 }
--- src/main/java/com/budwhite/studying/aspect/logging/service/FakeService.java
+++ src/main/java/com/budwhite/studying/aspect/logging/service/FakeService.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FakeService {
+  private final Logger logger = LoggerFactory.getLogger(FakeService.class);
 
-    private final Logger logger = LoggerFactory.getLogger(FakeService.class);
-    public String greet() {
-        logger.info("Doing some important shit in here");
-        return "Hello";
-    }
+  public String greet() {
+    logger.info("Doing some important shit in here");
+    return "Hello";
+  }
 }
